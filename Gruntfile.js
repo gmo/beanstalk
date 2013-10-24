904ec9a7384863ac023805bc9d5e8b4d6920cf10
Carson Full <carson.full@gmomail.org>
2013-10-24 11:37:36 -0500

    Added grunt file.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+	'use strict';
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		exec: {
+			composerInstall: {
+				cmd: 'composer install'
+			}
+		},
+
+		phpunit: {
+			classes: {
+				dir: 'tests/'
+			},
+			options: {
+				bin: 'vendor/bin/phpunit',
+				colors: true
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-phpunit');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-force-switch');
+
+	grunt.registerTask('default', function() { grunt.log(grunt.version); });
+	grunt.registerTask('build', ['exec:composerInstall']);
+	grunt.registerTask('test', ['build', 'phpunit']);
+	grunt.registerTask('test-force', ['turnForceOn', 'test', 'turnForceOff']);
+};
