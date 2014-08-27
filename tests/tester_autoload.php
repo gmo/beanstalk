4f1d5793fcc1f3290edc03c49b7ffc56a12c255f
Carson Full <carson.full@gmomail.org>
2014-08-27 17:43:51 -0500

    Updated composer and defining tests autoload in composer.json

--- tests/tester_autoload.php
+++ tests/tester_autoload.php
@@ -1,7 +1,6 @@
 <?php
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__ . "/../vendor/autoload.php";
-$loader->add( "", __DIR__ );
 
 define("WORKER_DIR", __DIR__ . "/workers");
 define("HOST", "127.0.0.1");
