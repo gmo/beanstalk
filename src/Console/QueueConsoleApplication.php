fb5f158ded230a1f53eb95662050015708fad846
Carson Full <carson.full@gmomail.org>
2014-10-23 12:31:41 -0500

    Added some default options back.

--- src/Console/QueueConsoleApplication.php
+++ src/Console/QueueConsoleApplication.php
@@ -28,10 +28,12 @@ class QueueConsoleApplication extends ConsoleApplication {
 		return new InputDefinition(array(
 			new InputArgument('command', InputArgument::REQUIRED, 'The command to execute'),
 
-			new InputOption('--help',   '-h', InputOption::VALUE_NONE,     'Display this help message.'),
 			new InputOption('host',     null, InputOption::VALUE_REQUIRED, 'Override beanstalk host'),
 			new InputOption('port',     null, InputOption::VALUE_REQUIRED, 'Override beanstalk port'),
 			new InputOption('dir',	    null, InputOption::VALUE_REQUIRED, 'Override worker directory'),
+			new InputOption('help',     '-h', InputOption::VALUE_NONE,     'Display this help message.'),
+			new InputOption('verbose', '-v|vv|vvv', InputOption::VALUE_NONE, 'Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug'),
+			new InputOption('version',  '-V', InputOption::VALUE_NONE, 'Display this application version.'),
 		));
 	}
 
