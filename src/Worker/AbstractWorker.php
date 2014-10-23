82ced0189585711472a1130fbf157cbc723f4195
Carson Full <carson.full@gmomail.org>
2014-10-23 12:53:30 -0500

    Added push method in worker as a shortcut.

--- src/Worker/AbstractWorker.php
+++ src/Worker/AbstractWorker.php
@@ -10,6 +10,18 @@ use GMO\Common\String;
  */
 abstract class AbstractWorker implements WorkerInterface {
 
+	/**
+	 * Shortcut for {@see TubeControlInterface::push} that uses the worker's tube name and ttr
+	 * @param TubeControlInterface $queue
+	 * @param \GMO\Common\ISerializable|\Traversable|array|mixed $data Job data
+	 * @param int|null $priority From 0 (most urgent) to 4294967295 (least urgent)
+	 * @param int|null $delay    Seconds to wait before job becomes ready
+	 * @return int The new job ID
+	 */
+	public static function push(TubeControlInterface $queue, $data, $priority = null, $delay = null) {
+		return $queue->push(static::getTubeName(), $data, $priority, $delay, static::getTimeToRun());
+	}
+
 	public static function getTubeName() {
 		return String::className(static::className());
 	}
