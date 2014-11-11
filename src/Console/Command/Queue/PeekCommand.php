edb0a5870d659c528f02a4ad89c4e14dffc9cd7b
Carson Full <carson.full@gmomail.org>
2014-11-11 12:14:53 -0600

    Added stats option to peek command to also run job stats command.

--- src/Console/Command/Queue/PeekCommand.php
+++ src/Console/Command/Queue/PeekCommand.php
@@ -17,36 +17,45 @@ class PeekCommand extends ChangeStateCommand {
 			->addTubeArgument()
 			->addStateOptions()
 			->addOption('id', 'i', InputOption::VALUE_REQUIRED, 'Inspect job by ID')
+			->addOption('stats', 's', InputOption::VALUE_NONE, 'Also output the job\'s stats')
 		;
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		if (!$id = $input->getOption('id')) {
-			parent::execute($input, $output);
-			return;
+		$this->ids = array();
+		if ($id = $input->getOption('id')) {
+			$this->ids[] = $id;
+			$this->validateState(false);
+			$this->validateTube(false);
 		}
-
-		$this->validateState(false);
-		$this->validateTube(false);
 
 		parent::execute($input, $output);
 
-		$job = $this->getQueue($input)->peekJob(intval($id));
-		$output->writeln("Peeking at job <info>#{$job->getId()}</info>");
-		$output->writeln(print_r($job->getData(), true));
+		if ($id) {
+			$job = $this->getQueue($input)->peekJob(intval($id));
+			$output->writeln("Peeking at job <info>#{$job->getId()}</info>");
+			$output->writeln(print_r($job->getData(), true));
+		}
+
+		if ($input->getOption('stats')) {
+			$this->renderStats($output, $this->ids);
+		}
 	}
 
 	protected function forEachTube(QueueInterface $queue, $tube, InputInterface $input, OutputInterface $output) {
 		if ($input->getOption('ready')) {
 			$job = $queue->peekReady($tube);
+			$this->ids[] = $job->getId();
 			$this->outputJob($output, $job, $tube, 'ready');
 		}
 		if ($input->getOption('buried')) {
 			$job = $queue->peekBuried($tube);
+			$this->ids[] = $job->getId();
 			$this->outputJob($output, $job, $tube, 'buried');
 		}
 		if ($input->getOption('delayed')) {
 			$job = $queue->peekDelayed($tube);
+			$this->ids[] = $job->getId();
 			$this->outputJob($output, $job, $tube, 'delayed');
 		}
 	}
@@ -55,4 +64,10 @@ class PeekCommand extends ChangeStateCommand {
 		$output->writeln("Peeking at the $state job <info>#{$job->getId()}</info> in <info>$tube</info> tube");
 		$output->writeln(print_r($job->getData(), true));
 	}
+
+	protected function renderStats(OutputInterface $output, array $ids) {
+		$this->callCommand($output, JobStatsCommand::NAME, array('id' => $ids));
+	}
+
+	protected $ids = array();
 }
