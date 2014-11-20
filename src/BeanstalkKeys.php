b38d8cd40de86eff1776ca82ea9f2f80c82e698a
Carson Full <carson.full@gmomail.org>
2014-11-20 15:55:21 -0600

    Prefixed all beanstalk keys

--- src/BeanstalkKeys.php
+++ src/BeanstalkKeys.php
@@ -5,12 +5,12 @@ final class BeanstalkKeys {
 
 	const HOST = 'beanstalk.host';
 	const PORT = 'beanstalk.port';
-	const WORKER_DIRECTORY = 'worker_manager.directory';
-	const QUEUE_LOGGER = 'queue.logger';
-	const QUEUE = 'queue';
-	const WEB_JOB_PRODUCER = 'queue.web_job_producer';
-	const WORKER_MANAGER_LOGGER = 'worker_manager.logger';
-	const WORKER_MANAGER = 'worker_manager';
+	const WORKER_DIRECTORY = 'beanstalk.worker_manager.directory';
+	const QUEUE_LOGGER = 'beanstalk.queue.logger';
+	const QUEUE = 'beanstalk.queue';
+	const WEB_JOB_PRODUCER = 'beanstalk.queue.web_job_producer';
+	const WORKER_MANAGER_LOGGER = 'beanstalk.worker_manager.logger';
+	const WORKER_MANAGER = 'beanstalk.worker_manager';
 
 	private function __construct() { }
 }
