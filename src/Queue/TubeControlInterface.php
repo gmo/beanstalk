426c9e94e90494ca56a67b65f623bbf98e21805b
Carson Full <carson.full@gmomail.org>
2014-12-16 09:12:48 -0600

    Deprecated old TubeControlInterface

---
+++ src/Queue/TubeControlInterface.php
@@ -0,0 +1,8 @@
+<?php
+namespace GMO\Beanstalk\Queue;
+
+/**
+ * @deprecated
+ * Use {@see \GMO\Beanstalk\Tube\TubeControlInterface} instead
+ */
+interface TubeControlInterface extends \GMO\Beanstalk\Tube\TubeControlInterface {}
