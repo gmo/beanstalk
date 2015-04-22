b77cce760bd9d4d6b4cb610e1160f3285dc868ad
Carson Full <carson.full@gmomail.org>
2015-04-21 19:41:09 -0500

    Updated and standardized runner log statements. Removing id, and data parameters since those will be added with JobProcessor.

--- src/Runner/BaseRunner.php
+++ src/Runner/BaseRunner.php
@@ -44,9 +44,10 @@ class BaseRunner implements RunnerInterface, LoggerAwareInterface {
 			throw new \LogicException('Setup method needs to be called before run');
 		}
 
-		$this->log->info("Running worker: " . $this->tubeName);
+		$this->log->info('Running worker');
 
 		$this->setupWorker($this->worker);
+		$this->log->debug('Finished setting up worker');
 
 		$job = new NullJob();
 		do {
@@ -55,7 +56,7 @@ class BaseRunner implements RunnerInterface, LoggerAwareInterface {
 				$this->processJob($job);
 			}
 		} while ($this->shouldKeepRunning());
-		$this->log->info('Stopping worker...');
+		$this->log->debug('Stopping worker...');
 		$this->worker->onStop();
 		$this->log->info('Worker stopped');
 	}
@@ -67,7 +68,6 @@ class BaseRunner implements RunnerInterface, LoggerAwareInterface {
 		}
 
 		if (!$this->validateJob($job)) {
-			$this->log->error('Job missing required params is being deleted!');
 			$job->delete();
 			return;
 		}
@@ -89,10 +89,7 @@ class BaseRunner implements RunnerInterface, LoggerAwareInterface {
 
 	public function preProcessJob(Job $job) {
 		if ($job instanceof UnserializableJob) {
-			$this->log->error('Burying unserializable job', array(
-				'id' => $job->getId(),
-				'data' => $job->getData(),
-			));
+			$this->log->error('Burying unserializable job');
 			$job->bury();
 		}
 		return $job;
@@ -106,9 +103,7 @@ class BaseRunner implements RunnerInterface, LoggerAwareInterface {
 		foreach ($this->worker->getRequiredParams() as $reqParam) {
 			if (!$params->containsKey($reqParam)) {
 				$this->log->error('Job is missing required parameter', array(
-					"missing" => $reqParam,
-					"id" => $job->getId(),
-					"data" => $job->getData(),
+					'missing' => $reqParam,
 				));
 				return false;
 			}
@@ -120,7 +115,7 @@ class BaseRunner implements RunnerInterface, LoggerAwareInterface {
 		if ($job->isHandled()) {
 			return;
 		}
-		$this->log->debug("Deleting the current job from: " . $this->tubeName);
+		$this->log->debug('Deleting finished job');
 		$job->delete();
 	}
 
@@ -135,11 +130,6 @@ class BaseRunner implements RunnerInterface, LoggerAwareInterface {
 
 	public function getJob(Job $previousJob) {
 		$this->checkForTerminationSignal();
-
-		# Only log if last call was valid to prevent spamming the log
-		if (!$previousJob instanceof NullJob) {
-			$this->log->debug("Getting next job...");
-		}
 
 		$job = $this->queue->reserve($this->tubeName, static::JOB_RESERVATION_TIMEOUT);
 
@@ -195,40 +185,33 @@ class BaseRunner implements RunnerInterface, LoggerAwareInterface {
 
 	protected function pauseTube($delay) {
 		$this->log->notice('Pausing tube', array(
-			'tube' => $this->tubeName,
 			'delay' => $delay,
 		));
 		$this->queue->pause($this->tubeName, $delay);
 	}
 
 	protected function buryJob(Job $job, $exception, $numErrors) {
-		$this->log->warning("Burying failed job", array(
-			"numErrors" => $numErrors,
-			"id"        => $job->getId(),
-			"data"      => $job->getData(),
-			"exception" => $exception
+		$this->log->warning('Burying failed job', array(
+			'numErrors' => $numErrors,
+			'exception' => $exception,
 		));
 		$job->bury();
 	}
 
 	protected function deleteJob(Job $job, $exception, $numErrors) {
-		$this->log->notice("Deleting failed job", array(
-			"numErrors" => $numErrors,
-			"id"        => $job->getId(),
-			"data"      => $job->getData(),
-			"exception" => $exception
+		$this->log->notice('Deleting failed job', array(
+			'numErrors' => $numErrors,
+			'exception' => $exception,
 		));
 		$job->delete();
 	}
 
 	protected function delayJob(Job $job, JobErrorInterface $jobError, $exception, $numErrors) {
 		$delay = !$jobError->shouldPauseTube() ? $jobError->getDelay($numErrors) : 0;
-		$this->log->notice("Delaying failed job", array(
-			"numErrors" => $numErrors,
-			"delay" 	=> $delay,
-			"id"        => $job->getId(),
-			"data"      => $job->getData(),
-			"exception" => $exception
+		$this->log->notice('Delaying failed job', array(
+			'numErrors' => $numErrors,
+			'delay'     => $delay,
+			'exception' => $exception,
 		));
 		$job->release($delay);
 	}
