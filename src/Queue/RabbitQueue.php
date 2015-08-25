9eeb259edd1294e4d11f40db169d98e161735dc2
Carson Full <carson.full@gmomail.org>
2015-08-24 21:22:46 -0500

    Some easy ones: touch, setLogger, tube

--- src/Queue/RabbitQueue.php
+++ src/Queue/RabbitQueue.php
@@ -256,36 +256,24 @@ class RabbitQueue implements QueueInterface {
 	}
 
 	/**
-	 * Allows a worker to request more time to work on a job.
-	 *
-	 * This is useful for jobs that potentially take a long time, but you still want
-	 * the benefits of a TTR pulling a job away from an unresponsive worker.  A worker
-	 * may periodically tell the server that it's still alive and processing a job
-	 * (e.g. it may do this on DEADLINE_SOON).
-	 *
-	 * @param Job $job
+	 * {@inheritdoc}
 	 */
 	public function touch(Job $job) {
-
+		//NOPE
 	}
 
 	/**
-	 * Sets a logger instance on the object
-	 *
-	 * @param LoggerInterface $logger
-	 * @return null
+	 * {@inheritdoc}
 	 */
 	public function setLogger(LoggerInterface $logger) {
-
+		$this->logger = $logger;
 	}
 
 	/**
-	 * Gets a tube by name
-	 * @param string $name
-	 * @return Tube
+	 * {@inheritdoc}
 	 */
 	public function tube($name) {
-
+		return new Tube($name, $this);
 	}
 
 	/**
